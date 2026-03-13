3d627c7aa361af86c3af6869a7de6ed966ca6105
Nick Dodsley <nick@ndestates.com>
2026-03-13 08:04:20 +0000

    fix: Configure tests to use Orchestra Testbench properly
    - Update TestCase to extend Orchestra Testbench TestCase
    - Configure service provider and database for testing
    - Update PHPUnit bootstrap to use Testbench bootstrap
    - Simplify CI environment setup for Testbench
    - Remove manual database creation (Testbench uses in-memory SQLite)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,26 @@
 
 namespace Tests;
 
-use PHPUnit\Framework\TestCase as BaseTestCase;
+use Orchestra\Testbench\TestCase as BaseTestCase;
+use NDEstates\LaravelModelSchemaChecker\ModelSchemaCheckerServiceProvider;
 
 abstract class TestCase extends BaseTestCase
 {
-    
+    protected function getPackageProviders($app)
+    {
+        return [
+            ModelSchemaCheckerServiceProvider::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        // Setup test environment
+        $app['config']->set('database.default', 'sqlite');
+        $app['config']->set('database.connections.sqlite', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+    }
 }
